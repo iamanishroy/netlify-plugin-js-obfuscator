62c88bd0596ecdb0cc57c3793c3329730e749b0c
Anish Roy <62830866+Anish6275@users.noreply.github.com>
2021-01-28 13:33:28 +0530

    issues fixed

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
-const fs = require("fs"); 
-const readdirp = require("readdirp"); 
-var JavaScriptObfuscator = require("javascript-obfuscator"); 
+const fs = require("fs");
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const readdirp = require("readdirp");
+const JavaScriptObfuscator = require("javascript-obfuscator");
+const excludedDir = ["!node_modules", "!plugins"];
 
-// listing all the '.js' files 
+// listing all the '.js' files
 const getJsFiles = async (directory, func_dir) => {
+  if (func_dir) {
+    excludedDir.push(`!${func_dir}`);
+  }
+  console.log(excludedDir);
   const files = await readdirp.promise(directory, {
     fileFilter: "*.js",
-    directoryFilter: ["!node_modules", "!plugins", `!${func_dir}`],
+    directoryFilter: excludedDir,
   });
   return files.map((file) => file.fullPath);
 };
 
 // Reading the files -> obfuscating the code -> Writing the obfuscated code
-const obfuscateCode = async (filePath, custom) => {
-  var file = fs.readFileSync(filePath, "utf8");
-  var obfuscationResult = await JavaScriptObfuscator.obfuscate(file, {
-    compact: custom[0],
-    controlFlowFlattening: true,
-    controlFlowFlatteningThreshold: 1,
-    numbersToExpressions: custom[1],
-    simplify: custom[2],
-    shuffleStringArray: custom[3],
-    splitStrings: custom[4],
-  });
-  await fs.writeFile(
-    filePath,
-    obfuscationResult.getObfuscatedCode(),
-    function (err) {
-      if (err) {
-        return 0;
-      }
-    }
-  );
+const obfuscateCode = async (filePath, custom, utils) => {
+  var file = await readFile(filePath, "utf8");
+  try {
+    var obfuscationResult = JavaScriptObfuscator.obfuscate(file, {
+      compact: custom[0],
+      controlFlowFlattening: true,
+      controlFlowFlatteningThreshold: 1,
+      numbersToExpressions: custom[1],
+      simplify: custom[2],
+      shuffleStringArray: custom[3],
+      splitStrings: custom[4],
+    });
+  } catch (error) {
+    return utils.build.failBuild(`Failed to Obfuscate '${filePath}' .`, { error });
+  }
+  await writeFile(filePath, obfuscationResult.getObfuscatedCode());
   return 1;
 };
 module.exports = {
@@ -40,20 +44,20 @@ module.exports = {
       constants.PUBLISH_DIR,
       constants.FUNCTIONS_SRC
     );
-    try {
-      for (const filePath of jsFiles) {
-        console.log(filePath);
-        await obfuscateCode(filePath, [
+    for (const filePath of jsFiles) {
+      console.log(filePath);
+      await obfuscateCode(
+        filePath,
+        [
           inputs.compact,
           inputs.numbersToExpressions,
           inputs.simplify,
           inputs.shuffleStringArray,
           inputs.splitStrings,
-        ]);
-      }
-      console.log("JS files successfully Obfuscated!");
-    } catch (error) {
-      return utils.build.failBuild("Failed to Obfuscate JS.", { error });
+        ],
+        utils
+      );
     }
+    console.log("JS files successfully Obfuscated!");
   },
 };
